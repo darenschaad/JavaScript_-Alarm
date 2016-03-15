c1019b693e20e1f23520e33d5fd1fef62ad47a0b
Daren Schaad and Mike Gallegos <student@epicodus.com>
2016-03-15 16:53:59 -0700

    add snooze

--- js/time-interface.js
+++ js/time-interface.js
@@ -2,19 +2,19 @@ var alarms = [];
 
 function startTime(){
   var time = moment().format('h:mm:ss a');
-
   document.getElementById('txt').innerHTML = time;
-
   setTimeout(startTime, 500);
 }
 
 function check(){
-  if (alarms.length === 1){
+  if (alarms.length >= 1){
     if (alarms[0].hour() === moment().hour() &&
         alarms[0].minute() === moment().minute() &&
         alarms[0].second() === moment().second()){
-      document.getElementById('youTube').click();    
+      // document.getElementById('youTube').click();
       console.log("ALARM IS GOING OFF");
+
+      // $('#dummy').append('<button id="snooze">Snooze</button>')
     } else {
       console.log("not going off");
     }
@@ -22,8 +22,8 @@ function check(){
     setTimeout(check, 1000);
 }
 
-
 $(document).ready(function(){
+
  $('#txt').text(startTime());
 
  check();
@@ -43,4 +43,12 @@ $(document).ready(function(){
     console.log(morningNight);
     alarms.push(firstAlarm);
   });
+
+  $('#snooze').click(function(){
+    debugger;
+    alarms[0] = moment().add(10, 'second');
+    console.log(alarms[0]);
+  });
+
+
 });
